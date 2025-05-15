89db287b3e5d57f2d053922e107d6bf0d0300c21
ewbriao1978 <eduardo.briao@gmail.com>
2025-05-15 14:42:10 -0300

    ok

--- app/Views/login.php
+++ app/Views/login.php
@@ -12,6 +12,20 @@
 
 </head>
 <body>
+    <?php
+    if (session()->getFlashdata('success')): ?>
+        <div class="alert alert-success" role="alert">
+            <?= session()->getFlashdata('success') ?>
+        </div>
+    <?php endif; ?>
+    <?php
+    if (session()->getFlashdata('error')): ?>
+        <div class="alert alert-danger" role="alert">
+            <?= session()->getFlashdata('error') ?>
+        </div>
+    <?php endif; ?>
+
+
     
     <div id="form" class="row justify-content-center">
        
